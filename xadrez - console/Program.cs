d1d9ecd7e4e96e354a5972fc8fe133b0c7dfb6ab
Camila Batista <Camila Batista>
2020-01-27 14:40:41 -0200

    Criando a classe Posicao

--- xadrez - console/Program.cs
+++ xadrez - console/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-
+using tabuleiro;
 namespace xadrez___console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+
+
         }
     }
 }
+
