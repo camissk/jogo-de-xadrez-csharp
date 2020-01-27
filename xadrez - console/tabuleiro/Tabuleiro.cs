5d1b5ae1e43f3994722670e24311c438cfedce98
Camila Batista <Camila Batista>
2020-01-27 16:00:34 -0200

    Colocando pecas no tabuleiro

--- xadrez - console/tabuleiro/Tabuleiro.cs
+++ xadrez - console/tabuleiro/Tabuleiro.cs
@@ -18,6 +18,12 @@
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
-        } //ok
+        } 
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
     }
 }
