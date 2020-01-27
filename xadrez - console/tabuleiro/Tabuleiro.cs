60403c805fa1fabcab5e387479511ed82e21e0d5
Camila Batista <Camila Batista>
2020-01-27 14:57:35 -0200

    Começando a implementar o tabuleiro

---
+++ xadrez - console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        //props
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        //construtor, criar tabulero e instanciar a matriz de peças e definir a quantidade de linhas e colunas 
+        public Tabuleiro(int linha, int coluna)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+
+        }
+    }
+}
